d362136a16f66effbaafff3a9f0bc4cee1c3b534
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-10-11 14:57:35 +0200

    chore: add razor runtime compilation

--- src/AspNetCoreMvc/Program.cs
+++ src/AspNetCoreMvc/Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+  .AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
@@ -22,6 +23,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
   name: "default",
-  pattern: "{controller=Home}/{action=Index}/{id?}");
+  pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
